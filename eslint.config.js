da33c8d005872cfd05d4e5c7f054f48457519fd5
H2OClanger <jwarton@yahoo.com>
2026-08-09 08:12:16 +0100

    fix(tooling): configure ESLint with installed TypeScript packages

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,31 @@
 import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 import globals from 'globals';
 
-export default tseslint.config(
+export default [
   {
     ignores: ['dist/**', 'coverage/**', 'node_modules/**']
   },
   js.configs.recommended,
-  ...tseslint.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module'
+      },
       globals: {
         ...globals.browser,
         ...globals.node
       }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules
     }
   }
-);
+];
